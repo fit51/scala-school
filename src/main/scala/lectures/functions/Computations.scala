1ab6fb4343d93ec7eb22877a0d09690882b2bddc
Your Name <fit511@yandex.ru>
2017-03-10 13:27:09 +0300

    Fixed Lecture2

--- src/main/scala/lectures/functions/Computations.scala
+++ src/main/scala/lectures/functions/Computations.scala
@@ -36,16 +36,17 @@ object Computation extends App with Data {
   *
   * Раскомментируйте последнюю строчку
   *
-  * Какой тип имеет partiallyAppliedCurriedFunction - ?
+  * Какой тип имеет partiallyAppliedCurriedFunction - ? (Array[String]) => Array[String]
   */
 object CurriedComputation extends App with Data {
 
-  def curriedComputation(filterData: String)(dataProducer: Array[String]): Array[String] = ???
+  def curriedComputation(filterData: String)(dataProducer: Array[String]): Array[String] =
+    dataProducer.filter(dataItem => filterData.contains(dataItem))
 
-  val partiallyAppliedCurriedFunction = ???
+  val partiallyAppliedCurriedFunction = curriedComputation(filterData)_
 
-  //val result = partiallyAppliedCurriedFunction(dataArray)
-  //result.foreach(print)
+  val result = partiallyAppliedCurriedFunction(dataArray)
+  result.foreach(print)
 }
 
 /**
@@ -53,11 +54,12 @@ object CurriedComputation extends App with Data {
   */
 object FunctionalComputation extends App with Data {
 
-  def functionalComputation(filterData: String): (Array[String]) => Array[String] = ???
+  def functionalComputation(filterData: String): (Array[String]) => Array[String] =
+    (dataProducer: Array[String]) => { dataProducer.filter(dataItem => filterData.contains(dataItem)) }
 
-  val filterApplied = functionalComputation(???)
+  val filterApplied = functionalComputation(filterData)
 
-  val result = filterApplied(???)
+  val result = filterApplied(dataArray)
   result.foreach(print)
 }
 

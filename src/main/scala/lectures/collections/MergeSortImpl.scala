fd6ed88cc2862c17ca931d012f28135ce8ed95cc
Your Name <fit511@yandex.ru>
2017-03-21 13:47:30 +0300

    HomeTask4

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -8,6 +8,22 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  def mergeSort(data: Seq[Int]): Seq[Int] = data
+  def mergeSort(data: Seq[Int]): Seq[Int] = {
+    val n = data.length/2
+    if(n == 0) data
+    else {
+      def merge(l1: Seq[Int], l2: Seq[Int]): List[Int] = (l1, l2) match {
+        case (Nil, ls2) => ls2.toList
+        case (ls1, Nil) => ls1.toList
+        case _ => {
+          if (l1.head < l2.head)
+            l1.head :: merge(l1.tail, l2)
+          else
+            l2.head :: merge(l1, l2.tail)
+        }
+      }
+      merge(mergeSort(data.take(n)), mergeSort(data.drop(n)))
+    }
+  }
 
 }

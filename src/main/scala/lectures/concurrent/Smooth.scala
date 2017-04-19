9913468b4658f1ce1757922cd65486cd1151ee0e
Your Name <fit511@yandex.ru>
2017-04-19 15:34:29 +0300

    HomeTask8

--- src/main/scala/lectures/concurrent/Smooth.scala
+++ src/main/scala/lectures/concurrent/Smooth.scala
@@ -1,6 +1,11 @@
 package lectures.concurrent
 
-import scala.concurrent.Future
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util._
 
 /**
   * Smooth - это своебразный функциональный кэш, предназначенный для исключения повторных вызовов кода
@@ -16,9 +21,47 @@ import scala.concurrent.Future
   *
   */
 object Smooth{
-  //def apply(thunk: =>): Smooth = ???
+  def apply[T](thunk: => T): Smooth[T] = new Smooth(thunk)
 }
 
-class Smooth {
-   def apply(): Future[_] = ???
+class Smooth[T](thunk: => T) {
+
+   var is_running = new AtomicBoolean(false)
+   var resut: AtomicReference[Future[T]] = new AtomicReference[Future[T]](null)
+
+   def apply(): Future[T] = if (is_running.get()) resut.get() else {
+      is_running.set(true)
+      val f = Future {
+         thunk
+      }
+      resut.set(f)
+      f onComplete {
+         case _ => is_running.set(false)
+      }
+      f
+   }
+}
+
+object SmoothExample extends App {
+   val executeCode1 = Smooth({
+      var i = 0
+      Thread.sleep(1000)
+      (Random.alphanumeric take 10 toList) mkString
+   })
+   var res1 = executeCode1()
+   res1 onSuccess {
+      case result => println(s"First res = $result")
+   }
+   val res2 = executeCode1()
+   res2 onSuccess {
+      case result => println(s"Second res = $result")
+   }
+   println("Calls during second are equal: " + (res1 eq res2))
+   Thread.sleep(1100)
+   val res3 = executeCode1()
+   res3 onSuccess {
+      case result => println(s"Third res = $result")
+   }
+   println("Calls after a second are equal: " + (res1 eq res3))
+   Await.result(res3, Duration.Inf)
 }
